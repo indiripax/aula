d6fd877c11dddca086d03823ccdd3ee79876ee10
indiripax <indicsmnamine@hotmail.com>
2025-09-12 20:56:48 -0300

    esses sim são da 3

---
+++ Lista de exercícios 3.py
@@ -0,0 +1,8 @@
+#1 criar uma tupla com as letras A, C, D, E, F, G, H, I, K, L, M, N, P, Q, R, S, T, V, W, Y 
+Letras = ('A' 'C' 'D' 'E' 'F' 'G' 'H' 'I' 'K' 'L' 'M' 'N' 'P' 'Q' 'R' 'S' 'T' 'V' 'W' 'Y')
+
+#a- Conte e imprima o número de elementos contidos na tupla criada.
+print(len(Letras))
+
+#b- Verifique e imprima se o aminoácido Serina (S) pertence à tupla. 
+#
