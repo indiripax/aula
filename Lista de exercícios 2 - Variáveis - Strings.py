d0fdb2054dbc2d2f9dbdfc80a197b4cae5aa0e91
indiripax <indicsmnamine@hotmail.com>
2025-09-13 20:04:54 -0300

    mais uma tentativa de fazer esse trem

--- Lista de exercícios 2 - Variáveis - Strings.py
+++ Lista de exercícios 2 - Variáveis - Strings.py
@@ -9,24 +9,21 @@ leucina= TNF.count("LL")
 print(leucina)
 
 #c- Encontre na sequência as posições ocupadas por duas glicinas (GG) e duas argininas (RR).
-tam = len(TNF)
-for i in (0,tam -1):
-    if TNF[i] == "G":
-        if TNF[i+1] == "G":
-            print(i)
+print(TNF.index("GG"))
 
-for i in (0, tam -1):
-    if TNF[i] == "R":
-        if TNF[i+1] =="r":
-            print(i)
+print(TNF.index("RR"))
 
 #d- Retorne os 100 primeiros aminoácidos da sequência.
 print(TNF[0:99])
 
 #e- Substitua o trecho da sequência com a ocorrência de 3 serinas e 1 arginina (SSSR) por alaninas.
-
+NovoTNF = TNF.replace("SSSR", "AAAA")
+print(NovoTNF)
 
 #f- Quebre a sequência no local onde a substituição foi realizada.
+
+#estou com dpuvida se era isso mesmo que era pra ser feito
+print(NovoTNF.split("AAAA"))
 
 #Exercicio 2
 text= "As proteínas são cadeias polipeptídicas formadas pela ligação peptídica entre resíduos de aminoácidos. Existem 20 tipos de aminoácidos comumente encontrados nos seres vivos. A esses aminoácidos, foram atribuídas abreviações de 3 letras e símbolos de 1 letra. As abreviações de 3 letras são bastante evidentes consistindo nas três primeiras letras do se nome."
@@ -54,6 +51,10 @@ print(len(insulin_signal))
 
 #b- Quebre a sequência no trecho “LLALLALWG".
 
+#Não entendi o que foi pedido aqui nem na outra:
+OutraSequencia = insulin_signal.split("LLALLAWG")
+print(OutraSequencia)
+MaisUmaSequencia = "LLALLAWG"
 
 #c- Concatene as sequências resultantes obtendo a seguinte sequência final MALWMRLLPPDPAAA.
 SequenciaFinal = "MALWMRLLPPDPAAA"
