cd89dabdb25a059a916b09b8815250abaea3156d
indiripax <indicsmnamine@hotmail.com>
2025-09-16 13:30:44 -0300

    Create Lista de exercícios 8.py

---
+++ Lista de exercícios 8.py
@@ -0,0 +1,18 @@
+#Para os exercícios desta lista, considere a seguinte lista de sequências e faça programas em Python que execute as tarefas descritas abaixo:
+
+Sequência A
+LRSSSQNSSDKPVAHVVANHQVEEQLEWLSQRANALLANGMDLKDNQLVVPADGLYLVYSQVLFKGQGCPDYVLLTHTV
+SLRSSSDK
+
+Sequência B
+KPAAHLIGDPSKQNSLLWRANTDRAFLQDGFSLSNNSLLVPTSGIYFVYSQVVFSGKAYSPKATSSPLYLAHEVQLFSS
+
+Sequência C
+CPQGKYIHPQNNSICCTKCHKGTYLYNDCPGPGQDTDCRECESGSFTASENHLRHCLSCSKCRKEMGQVEISSCTVDRD
+TVCGCR
+
+#1. Imprima apenas as sequências com 80 ou mais aminoácidos.
+#2. Imprima apenas as sequências cujo tamanho seja maior que a média de tamanho das sequências.
+#3. Imprima apenas as sequências que possuam pelo menos uma histidina (H) e nenhuma prolina (P).
+#4. Identifique e imprima a maior dentre as três sequências a seguir.
+#5. Imprima as três sequências em ordem crescente de tamanho.
