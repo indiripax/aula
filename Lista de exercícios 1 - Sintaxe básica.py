64abf12560015b435239a3983f38a6eddc7a4d94
indiripax <indicsmnamine@hotmail.com>
2025-09-16 12:45:48 -0300

    Add files via upload

---
+++ Lista de exercícios 1 - Sintaxe básica.py
@@ -0,0 +1,32 @@
+#1
+linguagem = "python"
+print(linguagem)
+
+#2
+a = 1500
+b = 335
+print(a+b)
+print(a*b)
+print(a-b)
+
+#3
+a1 = "Marie"
+b1 = " Currie "
+c1 = "!!!"
+print(a1 + b1 + c1)
+
+#4
+print(2 + 3 == 5)
+
+#5
+print(5 + 9 = 13)
+#nesse caso, nao funciona por nao ter o ==, além desse numero e o outro definirem se a expressao eh falsa ou verdadeira
+
+#6
+pi = 3.1415
+print(pi)
+
+#7
+pi = 3.1415
+  print(pi)
+#aqui, ha a diferenca de versoes do python, em uma versao, a que esta no laboratorio onde temos aula funciona, a do collab nao
