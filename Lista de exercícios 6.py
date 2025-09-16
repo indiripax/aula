e987c539d60cf1a6e52bd2bccf36ac769612901a
indiripax <indicsmnamine@hotmail.com>
2025-09-16 13:11:21 -0300

    Create Lista de exercícios 6.py

---
+++ Lista de exercícios 6.py
@@ -0,0 +1,30 @@
+#1. O Protein Data Bank (PDB) é uma base de dados de estruturas de proteínas. Cada entrada da base de
+#dados é identificada de por um id de 4 letras / dígitos como por exemplo “1ABC”. Abaixo são apresentados
+#ids de estruturas PDBs, juntamente com a a contagem de aminoácidos presentes nessas estruturas.
+1A8M - 471
+1TNR - 283
+2AZ5 - 592
+1TNF - 471
+2TNF - 468
+2TUN - 942
+4TSV - 150
+5TSW - 900
+2E7A - 471
+6RMJ - 489
+
+#a) Construa um dicionário onde os ids PDB sejam as chaves e o número de resíduos de aminoácidos sejam o valor.
+
+
+#b) Imprima os valores contidos nos ids 2TNF e 2E7A.
+
+
+#c) Verifique e imprima o tamanho do dicionário construído
+
+
+#d) Obtenha e imprima a lista de todas as chaves do dicionário
+
+
+#e) Obtenha e imprima a lista de todos os valores do dicionário
+
+
+#f) Obtenha e imprima uma lista de tuplas com todos os pares chave-valor. Cada dupla será um par ordenado (chave, valor).
