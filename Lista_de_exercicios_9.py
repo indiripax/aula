51a614424e2d6f56bdfacbd6e9439bda194322d8
indiripax <indicsmnamine@hotmail.com>
2025-09-18 14:50:14 -0300

    bla

--- Lista_de_exercicios_9.py
+++ Lista_de_exercicios_9.py
@@ -1,18 +1,36 @@
 #1. Escreva um programa que calcule e escreva a tabela de graus Centígrados em função dos graus Farenheit que variem entre 1 e 150 de 1 em 1 conforme a fórmula a seguir
+print("Exercício 1")
+F = 100
 Celcios = 5/9*(F - 32)
 
+print(Celcios)
+
 #2. Utilizando as estruturas de repetição, verifique se as seguintes sequências são uma sequência de 
-DNA = {A,G, T, C}
-RNA = {U, C, A, G}
-PROTEÍNA = {A, C, D, E, F, G, H, I, K, L, M, N, P, Q, R, S, T, V, W, Y} 
+DNA = {"A", "G", "T", "C"}
+RNA = {"U", "C", "A", "G"}
+PROTEÍNA = {"A", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "Y"} 
 
 #ou nenhuma delas (nesse caso, imprima as letras que não fazem parte do alfabeto):
 s1 = "ATCDLASKWNWNHTLCAAHCIARRYRGGYCNSKAVCVCRN"
 s2 = "TATTAACCGGGTTTAAACTAGCATGCATGATTAACCAGTACATCTTTT"
 s3 = "ATCBDLASKWXWNHTLCAAHCIARRYRGGYCNSJAVCVCRN"
 
+
+
 #3. Utilizando as estruturas de repetição, gere e imprima a sequência complemento reverso da seguinte sequência de DNA:
-TATTAACCGGGTTTAAACTAGCATGCATGATTAACCAGTACATCTTTT
+print("Exercício 3")
+DNA = "TATTAACCGGGTTTAAACTAGCATGCATGATTAACCAGTACATCTTTT"
+DNA_COMPLEMENTO = ""
+for letra in DNA:
+    if letra == "A":
+            DNA_COMPLEMENTO += "T"
+    elif letra == "T":
+        DNA_COMPLEMENTO += "A"
+    elif letra == "C":
+        DNA_COMPLEMENTO += "G"
+    elif letra == "G":
+        DNA_COMPLEMENTO += "C"
+print(DNA_COMPLEMENTO[::-1])
 
 #4.Escreva um programa que calcule o fatorial de um número recebido como entrada. Lembre-se que o fatorial de um número é igual ao número multiplicado n sucessivamente por n-1, n-2, .. 1. 
 #Exemplo: fat(6) = 6 x 5 x 4 x 3 x 2 x 1 = 720.
