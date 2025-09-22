8009caf5e21efeeaa932e4cb358f914241d11070
indiripax <indicsmnamine@hotmail.com>
2025-09-22 20:06:37 -0300

    não sei mais o q está acontecendo

--- Lista_de_exercicios_9.py
+++ Lista_de_exercicios_9.py
@@ -74,20 +74,21 @@ print(f"A massa molar da sequência é {soma:.5f}")
 
 #7. Considerando a Tabela 2 que contem dados de defensinas de plantas, obtenha / calcule e imprima:
 
-pdb = {"3PSM":(1, "A,B", "UniProt", "Q6B519", "KTCENLADTFRGPCFTDGSCDDHCKNKEHLIKGRCRDDFRCWCTRNC", 47, "polipeptide(L)", 5.511),\
-    "2NY9": (1, "X", "Uniprot", "Q17027", "ATCDLASGFGVGSSLCAAHCLVKGYRGGYCKNKICHCRDKF", 41, "polypeptide(L)", 4.349),/
-    "2NY8": (1, "X", "Uniprot", "Q17027", "ATCDLASGFGVGSSLCAAHCIARRYRGGYCNSKAVCVCRN", 40, "polypeptide(L)", 4.148),/
-    "2NZ3": (1, "A", "UniProt", "Q17027", "ATCDLASIFNVNHALCAAHCIARRYRGGYCNSKAVCVCRN", 40, "polypeptide(L)", 4.353),/
-    "2E3G": (1, "A", "UniPort", "Q17027", "ATCDLASKWNWNHTLCAAHCIARRYRGGYCNSKAVCVCRN", 40, "polypeptide(L)", 4.525),/
-    "2E3F": (1, "A", "UniPort", "Q17027", "ATCDLASFSSQWVTPNDSLCAAHCIARRYRGGYCNGKRVCVCR", 43, "polypeptide(L)", 4.747)/
-    "2E3E": (1, "A", "UniPort", "Q17027", "ATCDLASFSSQWVTPNDSLCAAHCLVKGYRGGYCKNKICHCRDKF", 45, "polypeptide(L)", 5.007)}
+pdb = {"3PSM":(1, "A,B", "UniProt", "Q6B519", "KTCENLADTFRGPCFTDGSCDDHCKNKEHLIKGRCRDDFRCWCTRNC", 47, "polipeptide(L)", 5.511), "2NY9": (1, "X", "Uniprot", "Q17027", "ATCDLASGFGVGSSLCAAHCLVKGYRGGYCKNKICHCRDKF", 41, "polypeptide(L)", 4.349),    "2NY8": (1, "X", "Uniprot", "Q17027", "ATCDLASGFGVGSSLCAAHCIARRYRGGYCNSKAVCVCRN", 40, "polypeptide(L)", 4.148), "2NZ3": (1, "A", "UniProt", "Q17027", "ATCDLASIFNVNHALCAAHCIARRYRGGYCNSKAVCVCRN", 40, "polypeptide(L)", 4.353), "2E3G": (1, "A", "UniPort", "Q17027", "ATCDLASKWNWNHTLCAAHCIARRYRGGYCNSKAVCVCRN", 40, "polypeptide(L)", 4.525), "2E3F": (1, "A", "UniPort", "Q17027", "ATCDLASFSSQWVTPNDSLCAAHCIARRYRGGYCNGKRVCVCR", 43, "polypeptide(L)", 4.747), "2E3E": (1, "A", "UniPort", "Q17027", "ATCDLASFSSQWVTPNDSLCAAHCLVKGYRGGYCKNKICHCRDKF", 45, "polypeptide(L)", 5.007)}
 
+print("Exercício 7")
 #A) a menor sequência e seu comprimento
+print("a:")
+
+
+
 #B) a maior sequência e seu comprimento
+
 #C) a média entre os comprimentos das sequências
+
 #D) a mediana entre os comprimentos das sequências
 
 #8. Considerando as assinaturas moleculares (fingerprints) hipotéticas das tabelas abaixo, calcule a distância de Tanimoto
 
-
-batata
+fingerprint_1 = [0,0,1,0,0,1,1,0,1,1,0,1]
+fingerprint_2 = [0,1,0,0,1,1,0,1,1,0,0,0]
