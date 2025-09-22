9a64c364413d4458d7468845789058e8d5baa6f9
indiripax <indicsmnamine@hotmail.com>
2025-09-22 16:14:34 -0300

    tá indo

--- Lista_de_exercicios_9.py
+++ Lista_de_exercicios_9.py
@@ -63,9 +63,14 @@ Sequencia = "VRSSSRTPSDKPVAHVVANPQAEGQLQWLNRRANALLANGVELRDNQLVVPSEGLYLIYSQVLFKGQ
 
 Massa_aminoacidos = {"A":(71.03711), "C":(103.00919), "D":(115.02694), "E":(129.04259), "F":(147.06841), "G":(57.02146), "H":(137.05891), "I":(113.08406), "K":(128.09496), "L":(113.08406), "M":(131.04049), "N":(114.04293), "P":(97.05276), "Q":(128.05858), "R":(156.10111), "S":(87.03203), "T":(101.04768), "V":(99.06841), "W":(186.07931), "Y":(163.06333)}
 
+sequencia = list(Sequencia)
+
 soma = 0
 
-print(f"A massa molar da sequência é {soma}.")
+for aminoacido in sequencia:
+    soma += Massa_aminoacidos[aminoacido]
+
+print(f"A massa molar da sequência é {soma:.5f}")
 
 #7. Considerando a Tabela 2 que contem dados de defensinas de plantas, obtenha / calcule e imprima:
 
